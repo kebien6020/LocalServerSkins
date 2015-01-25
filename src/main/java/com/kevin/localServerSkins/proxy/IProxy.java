addab218e849dbc714b8f70bd7f57722589a35df
kebien6020 <kebien6020@hotmail.com>
2015-01-25 11:49:19 -0500

    Initial commit

---
+++ src/main/java/com/kevin/localServerSkins/proxy/IProxy.java
@@ -0,0 +1,9 @@
+package com.kevin.localServerSkins.proxy;
+
+import net.minecraft.util.ResourceLocation;
+
+public interface IProxy {
+
+	public void changeSkin(ResourceLocation newSkin);
+	
+}
---
+++ src/main/java/com/kevin/localServerSkins/proxy/ServerProxy.java
@@ -0,0 +1,12 @@
+package com.kevin.localServerSkins.proxy;
+
+import net.minecraft.util.ResourceLocation;
+
+public class ServerProxy extends CommonProxy {
+	
+	@Override
+	public void changeSkin(ResourceLocation newSkin) {
+		// In the server do nothing
+	}
+
+}
