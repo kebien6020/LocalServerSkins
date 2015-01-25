cc2252268ea8cc4dfb0568a13938b02c32cb9cc6
kebien6020 <kebien6020@hotmail.com>
2015-01-25 12:30:18 -0500

    Eclipse commit

--- src/main/java/com/kevin/localServerSkins/proxy/IProxy.java
+++
@@ -1,9 +0,0 @@
-package com.kevin.localServerSkins.proxy;
-
-import net.minecraft.util.ResourceLocation;
-
-public interface IProxy {
-
-	public void changeSkin(ResourceLocation newSkin);
-	
-}
--- src/main/java/com/kevin/localServerSkins/reference/Reference.java
+++
@@ -1,7 +0,0 @@
-package com.kevin.localServerSkins.reference;
-
-public class Reference {
-	public static final String MOD_ID = "LocalServerSkins";
-	public static final String MOD_NAME = "Local Server Skins";
-	public static final String VERSION = "0.0.0";
-}
